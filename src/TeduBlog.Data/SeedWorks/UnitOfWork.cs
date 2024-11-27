3e7fedbd7c35a25edada13f80836ed43e460460d
Hungnt29x1 <hungnt29.x1@gmail.com>
2024-11-27 15:37:26 +0700

    Configuration Repository và UnitOfWork

---
+++ src/TeduBlog.Data/SeedWorks/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using TeduBlog.Core.SeedWorks;
+
+namespace TeduBlog.Data.SeedWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TeduBlogContext _context;
+        public UnitOfWork(TeduBlogContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CompleteAsync()
+        {
+            // Ghi các thay đổi xuống cơ sở dữ liệu
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            // Giải phóng tài nguyên
+            _context.Dispose();
+        }
+    }
+}
