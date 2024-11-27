9bda20ff1637bb79a45389394f3088f8837eac4b
Hungnt29x1 <hungnt29.x1@gmail.com>
2024-11-27 15:54:52 +0700

    test repository for Post

---
+++ src/TeduBlog.Data/Repositories/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeduBlog.Core.Domain.Content;
+using TeduBlog.Core.Repositories;
+using TeduBlog.Data.SeedWorks;
+
+namespace TeduBlog.Data.Repositories
+{
+    public class PostRepository : RepositoryBase<Post, Guid>, IPostRepository
+    {
+        public PostRepository(TeduBlogContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Post>> GetPopularPostsAsync(int count)
+        {
+            return await _context.Posts.OrderByDescending(x => x.ViewCount).Take(count).ToListAsync();
+        }
+    }
+}
