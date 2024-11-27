bb4130dda2ee9ed02911e6b2a2579317698d8116
Hungnt29x1 <hungnt29.x1@gmail.com>
2024-11-27 17:10:44 +0700

    Test Controller For Post

--- src/TeduBlog.Data/SeedWorks/UnitOfWork.cs
+++ src/TeduBlog.Data/SeedWorks/UnitOfWork.cs
@@ -1,14 +1,21 @@
-﻿using TeduBlog.Core.SeedWorks;
+﻿using AutoMapper;
+using TeduBlog.Core.Repositories;
+using TeduBlog.Core.SeedWorks;
+using TeduBlog.Data.Repositories;
 
 namespace TeduBlog.Data.SeedWorks
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly TeduBlogContext _context;
-        public UnitOfWork(TeduBlogContext context)
+        public UnitOfWork(TeduBlogContext context, IMapper mapper)
         {
             _context = context;
+            Posts = new PostRepository(context, mapper);
         }
+
+        public IPostRepository Posts { get; private set; }
+
         public async Task<int> CompleteAsync()
         {
             // Ghi các thay đổi xuống cơ sở dữ liệu
