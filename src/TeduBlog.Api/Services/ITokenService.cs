1a28cad32ecc31afe0dd746421bc01f1d666aa1e
Hungnt29x1 <hungnt29.x1@gmail.com>
2024-12-02 18:25:47 +0700

    authen and author

---
+++ src/TeduBlog.Api/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace TeduBlog.Api.Services
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);//Get thông tin cá nhân từ ExpiredToken
+    }
+}
---
+++ src/TeduBlog.Core/ConfigOptions/JwtTokenSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TeduBlog.Core.ConfigOptions
+{
+    public class JwtTokenSettings
+    {
+        public string Key { get; set; } // => để mã hóa
+        public string Issuer { get; set; } // => đơn vị mã hóa là ai
+        public int ExpireInHours { get; set; } // hết hạn sau bao lâu
+    }
+}
